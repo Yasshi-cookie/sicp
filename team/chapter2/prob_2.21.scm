d0c8eda24a6ed5d5f3c11b0067d65c82b0ace2d6
Yasshi-cookie <y.teruyacookie@gmail.com>
2022-04-10 20:28:56 +0900

    4月10日（日）の分

---
+++ team/chapter2/prob_2.21.scm
@@ -0,0 +1,13 @@
+(define nil '())
+
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons (square (car items))
+            (square-list (cdr items)))))
+(define (square-list items)
+  (map square items))
+
+(square-list (list 1 2 3 4))
+
+(cons nil 1)
