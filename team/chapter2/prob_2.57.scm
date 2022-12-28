b74aafa86cf8d482bafc9009f8f8e48063462a95
Yasshi-cookie <y.teruyacookie@gmail.com>
2022-12-28 22:48:30 +0900

    12月28日（水）の分

---
+++ team/chapter2/prob_2.57.scm
@@ -0,0 +1,68 @@
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp) (if (same-variable? exp var) 1 0))
+        ((sum? exp) (make-sum (deriv (addend exp) var)
+                              (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product (make-product
+                        (exponent exp)
+                        (make-exponentiation (base exp) (- (exponent exp) 1)))
+                       (deriv (base exp) var)))
+        (else
+         (error "unknown expression type: DERIV" exp))))
+
+; • 変数は記号である。基本述語 symbol? で識別する。
+(define (variable? x) (symbol? x))
+; • ⼆つの変数は、それらを表現する記号が eq? であれば等しい。
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+; • 和と積は、リストとして構築する。
+(define (make-sum a1 a2) (list '+ a1 a2))
+(define (make-product m1 m2) (list '* m1 m2))
+; • 和は、最初の要素が記号 + であるリストである。
+(define (sum? x) (and (pair? x) (eq? (car x) '+)))
+; • 加数は、和のリストの⼆つ⽬の項である。
+(define (addend s) (cadr s))
+; • 被加数は、和のリストの三つ⽬の項である。
+; (define (augend s) (caddr s))
+(define (augend s)
+  (cond ((= (length (cddr s)) 1) (caddr s))
+        (else (append (list (car s)) (cddr s)))))
+(augend '(+ x y z))
+; (+ y z)
+
+; • 積は、最初の要素が記号 * であるリストである。
+(define (product? x) (and (pair? x) (eq? (car x) '*)))
+; • 乗数は、積のリストの⼆つ⽬の項である。
+(define (multiplier p) (cadr p))
+; • 被乗数は、積のリストの三つ⽬の項である。
+; (define (multiplicand p) (cddr p))
+(define (multiplicand p)
+  (cond ((= (length (cddr p)) 1) (caddr p))
+        (else (append (list (car p)) (cddr p)))))
+
+(define (=number? exp num)
+  (and (number? exp)
+       (= exp num)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list '* m1 m2))))
+
+(deriv '(* x y (+ x 3)) 'x)
