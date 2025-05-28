0dce670cbbc6953858902d367f0fc3acee6a6dcb
Yasshi-cookie <y.teruyacookie@gmail.com>
2025-05-28 21:26:59 +0900

    5月28日の分

--- team/chapter3/prob_3.5.scm
+++ team/chapter3/prob_3.5.scm
@@ -1,4 +1,6 @@
-(define rand (let ((x random-init))
-               (lambda ()
-                 (set! x (rand-update x))
-                 x)))
+(random-integer 100)
+
+(define (estimate-integral p x1 x2 y1 y2 steps)
+  (let ((x (random-real x1 x2))
+        (y (random-real y1 y2)))
+    ))
